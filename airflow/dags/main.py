03f25ae2af3ffb782bd706836422ebb6e93b919b
angelospi <angelo.spinardi@hotmail.com>
2023-03-28 14:09:24 -0300

    Add CI

--- airflow/dags/main.py
+++ airflow/dags/main.py
@@ -1,9 +1,10 @@
-from airflow import DAG
-from airflow.operators.python import PythonOperator
-from datetime import datetime, timedelta
-import httpx
 import os
 import sys
+from datetime import datetime
+
+from airflow.operators.python import PythonOperator
+
+from airflow import DAG
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.join(current_dir, "../../"))
@@ -12,25 +13,33 @@ from scripts.transform import Transform
 
 dag = DAG(dag_id="projeto_agro", start_date=datetime(2023, 1, 13))
 
-def extract(**kwargs):
-    extract = Extract()
-    dict_files_name=extract.save_data_raw()
 
-    kwargs["ti"].xcom_push(
-        key="dict_files_name", value=dict_files_name
-    )
+def extract(**kwargs):
+    """
+    Extract data from api
+    """
+    extract_class = Extract()
+    dict_files_name = extract_class.save_data_raw()
+
+    # Pass dictionary with files names to stage transform
+    kwargs["ti"].xcom_push(key="dict_files_name", value=dict_files_name)
+
 
 def transform(**kwargs):
+    """
+    Stage transform data
+    """
     dict_files_name = kwargs["ti"].xcom_pull(
         key="dict_files_name", task_ids="extract_data"
     )
-    transform=Transform(dict_files_name)
+    transform_class = Transform(dict_files_name)
 
-    transform.separate_data_faostat()
-    transform.transform_columns()
-    transform.clean_dataset()
-    transform.merge_dataframes()
-    transform.send_data_to_staging()
+    transform_class.separate_data_faostat()
+    transform_class.transform_columns()
+    transform_class.clean_dataset()
+    transform_class.merge_dataframes()
+    transform_class.send_data_to_staging()
+
 
 extract = PythonOperator(task_id="extract_data", python_callable=extract, dag=dag)
 transform = PythonOperator(task_id="transform_data", python_callable=transform, dag=dag)
